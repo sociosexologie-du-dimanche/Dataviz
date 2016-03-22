c2a5e646afe47700c134183faa2cac1142b0c152
Roamdam <romain.damian@ntymail.com>
2016-03-22 05:59:50 +0100

    Ajouté un autre quizz

--- root/js/animations.js
+++ root/js/animations.js
@@ -200,3 +200,12 @@
     });
  })();
 
+//ScrollTO
+			$(document).ready(function() {
+				$('.scrollTo').click( function() { // Au clic sur un élément
+					var page = $(this).attr('href'); // Page cible
+					var speed = 900; // Durée de l'animation (en ms)
+					$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+					return false;
+				});
+			});
